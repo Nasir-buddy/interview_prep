c30a600ad13d8d789ad76d5184244b4aa7845304
nasir-buddy <syedali172000@gmail.com>
2025-01-31 00:31:37 +0530

    initial commit

---
+++ basic.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class basic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("The elements of the array are:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
